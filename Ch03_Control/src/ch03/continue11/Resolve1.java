ab440c468a3378d707729f4d6da5cab99e44dfe2
KimRosa <loveurosa0821@gmail.com>
2024-01-29 10:25:38 +0900

    ch03.continue11.Resolve01~03

--- Ch03_Control/src/ch03/continue11/Resolve1.java
+++ Ch03_Control/src/ch03/continue11/Resolve1.java
@@ -1,15 +1,16 @@
 package ch03.continue11;
 
+// for문 1~100까지 정수중에 3의 배수의 총합 출력
 public class Resolve1 {
 	public static void main(String[] args) {
-		int plus = 0;
-		for(int i = 1; i<=100; i++) {
+		int sum = 0;
+		for(int i=1;i<=100;i++) {
 			if(i%3==0) {
-				plus+=i;
 				
+				sum += i;
 			}
-			System.out.println(plus);
 		}
+		System.out.println("1~100까지 정수중에 3의 배수의 총합 :"
+				+ sum);
 	}
-
 }
--- Ch03_Control/src/ch03/continue11/Resolve2.java
+++ Ch03_Control/src/ch03/continue11/Resolve2.java
@@ -4,11 +4,11 @@ public class Resolve2 {
 	public static void main(String[] args) {
 		
 		
-		for (int i= 1; i<=5; i++) {
-			for (int j=0; j<i; j++)
-				System.out.print("*");
+		for (int i=1; i<=5; i++) {			// 행의 변화
+			for (int j=0; j<i; j++) {		// 열의 변화
+				System.out.print("*");		
+			}	
+			System.out.println();
+		}
 	}
-		System.out.println();
-
-}
 }
