475eeb4b71953baa13bb9e6e82fcf942f4d01a2d
KimRosa <108081283+KimRosa@users.noreply.github.com>
2024-01-30 15:00:07 +0900

    ch08.class03;Arith

--- Ch08_Class/src/ch08/class03/Arith.java
+++ Ch08_Class/src/ch08/class03/Arith.java
@@ -1,5 +1,12 @@
 package ch08.class03;
 
+/*
+ * 객체가 최초 생성될 때 비로소 메서드가 메모리에 올라간다. 
+ * 객체가 있어야 사용 가능하다. 
+ * 
+ * 객체의 변수값에 따라 영향을 받는 메서드는 일반 메서드로 만든다. 
+ * 
+ */
 public class Arith {
 	public static int add(int num0, int num1) {
 		return num0 + num1;
